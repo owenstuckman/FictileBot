a38b211fa1bd2f94eeaa3c9cd0758297123c1c04
PC <ostuckman@outlook.com>
2022-02-22 16:02:44 -0500

    finished golf bot code - w/o correct servo positions

--- TeamCode/src/main/java/robotx/modules/GolfMotorServo.java
+++ TeamCode/src/main/java/robotx/modules/GolfMotorServo.java
@@ -2,41 +2,62 @@ package robotx.modules;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import robotx.libraries.XModule;
 
 public class GolfMotorServo extends XModule {
 
-    DcMotor intakeMotor;
+    DcMotor liftMotor;
+    DcMotor dropperMotor;
 
-
+    Servo liftServo;
 
     double power = 1;
-
-
 
     public GolfMotorServo (OpMode op){
         super(op);
     }
 
     public void init() {
-        intakeMotor = opMode.hardwareMap.dcMotor.get("IntakeMotor");
+        liftMotor = opMode.hardwareMap.dcMotor.get("LiftMotor");
+        dropperMotor = opMode.hardwareMap.dcMotor.get("DropperMotor");
 
+        liftServo = opMode.hardwareMap.servo.get("IntakeMotor");
     }
 
 
 
     public void loop() {
         if (xGamepad2().x.isDown()) {
-            intakeMotor.setPower(power);
+            liftMotor.setPower(power);
         }
 
         else if (xGamepad2().y.isDown()) {
-            intakeMotor.setPower(-power);
+            liftMotor.setPower(-power);
         }
 
         else {
-            intakeMotor.setPower(0.0);
+            liftMotor.setPower(0.0);
+        }
+
+        if (xGamepad2().a.isDown()) {
+            dropperMotor.setPower(power);
+        }
+
+        else if (xGamepad2().b.isDown()) {
+            dropperMotor.setPower(-power);
+        }
+
+        else {
+            dropperMotor.setPower(0.0);
+        }
+
+        if (xGamepad2().right_stick_button.wasPressed()){
+            liftServo.setPosition(1);
+        }
+        if (xGamepad2().left_stick_button.wasPressed()){
+            liftServo.setPosition(0);
         }
 
 
